1353e795aca31811c89748f848efb9cd5d5cd919
williamye <william.ye@oocl.com>
2019-07-15 18:03:34 +0800

    [feat: refactor priceCalculator return code divide three double variable]

--- src/main/java/com/tws/refactoring/extract_variable/PriceCalculator.java
+++ src/main/java/com/tws/refactoring/extract_variable/PriceCalculator.java
@@ -3,9 +3,9 @@ package com.tws.refactoring.extract_variable;
 public class PriceCalculator {
     double getPrice(int quantity, int itemPrice) {
         // Price consists of: base price - discount + shipping cost
-//        double basePrice = quantity * itemPrice;
-//        double discount = Math.max(0, quantity - 500) * itemPrice * 0.05;
-//        double shippingCost = Math.min(quantity * itemPrice * 0.1, 100.0);
-//        return basePrice - discount + shippingCost;
+        double basePrice = quantity * itemPrice;
+        double discount = Math.max(0, quantity - 500) * itemPrice * 0.05;
+        double shippingCost = Math.min(quantity * itemPrice * 0.1, 100.0);
+        return basePrice - discount + shippingCost;
     }
 }
